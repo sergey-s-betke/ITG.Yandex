c175a5152a600b5961b27d3c24b7dd8d84f42d83
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-11-11 00:09:07 +0400

    * 1.0.0 + Get-Token + Get-CachedToken + Invoke-API
    Signed-off-by: Sergey S. Betke <sergey.s.betke@yandex.ru>

---
+++ ITG.PrepareModulesEnv.ps1
@@ -0,0 +1,34 @@
+﻿<#
+	.Synopsis
+		Скрипт, вызываемый при загрузке модулей ITG. Выполняет подготовку для загрузки модулей ITG.
+	.Description
+		Скрипт, вызываемый при загрузке модулей ITG. Выполняет подготовку для загрузки модулей ITG.
+		На данном этапе:
+		- добавляет в $env:PSModulePath каталог загружаемого модуля, чтобы все зависимости могли быть
+		импортированы без указания полного пути (то есть - в том числе и из общего репозитория модулей
+		powerShell)
+	.Link
+		описание манифеста модуля: http://get-powershell.com/post/2011/04/04/How-to-Package-and-Distribute-PowerShell-Cmdlets-Functions-and-Scripts.aspx
+#>
+[CmdletBinding(
+)]
+param ()
+
+Write-Verbose "Добавляем каталог $( Split-Path -Path ( $myinvocation.mycommand.path ) -Parent ) в `$Env:PSModulePath.";
+
+$Env:PSModulePath = (
+	@( $env:psmodulepath -split ';' ) `
+	+ ( Split-Path -Path ( $myinvocation.mycommand.path ) -Parent ) `
+	| Select-Object -Unique `
+) -join ';';
+
+Write-Verbose @"
+Изменённое значение `$Env:PSModulePath:
+$(
+(
+	@( $env:psmodulepath -split ';' ) `
+	| %{ "`t" + $_ } `
+) -join "`n"
+)
+"@
+;
